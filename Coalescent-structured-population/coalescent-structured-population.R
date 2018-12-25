f409512ff19ea184dc773958c1d340b6cc8cbb98
kholsinger <keholsinger@gmail.com>
2018-12-25 17:06:03 -0500

    New version with population and allele identity displayed simultaneously.

--- Coalescent-structured-population/coalescent-structured-population.R
+++ Coalescent-structured-population/coalescent-structured-population.R
@@ -1,14 +1,14 @@
 library(ggplot2)
+library(cowplot)
 library(coala)
 library(ggtree)
 library(ape)
-library(phyclust)
 
 rm(list = ls())
 
-pop_config <- c(5, 5, 5, 5, 5)
-migration_rate <- 0.1
-mutation_rate <- 0.1
+pop_config <- c(5, 5)
+migration_rate <- 2.0
+mutation_rate <- 2.0
 loci_number <- 1
 locus_length <- 1000
 
@@ -37,6 +37,7 @@ model <- coal_model(sample_size = pop_config,
   feat_mutation(rate = mutation_rate,
                 model = "IFS") +
   sumstat_seg_sites() +
+  sumstat_jsfs() +
   sumstat_trees()
 results <- simulate(model)
 
@@ -56,34 +57,54 @@ haplos_char <- character(nrow(snps))
 for (i in 1:nrow(snps)) {
   haplos_char[i] <- get_char(snps[i,], haplos)
 }
-## collect information for the tree
-##
-group_info <- split(tree$tip.label, haplos_char)
-snp_groups <- groupOTU(tree, group_info)
-## plot the tree with different colors for different alleles
-##
-p <- ggtree(snp_groups, aes(color = group)) +
-  scale_color_brewer(palette = "Dark2") +
-  geom_tiplab(size = 1.5) +
-  ggtitle("Colored by alleles")
-print(p)
-
-readline(prompt = "Press [enter] to continue")
+if (0) {
+  ## collect information for the tree
+  ##
+  group_info <- split(tree$tip.label, haplos_char)
+  snp_groups <- groupOTU(tree, group_info)
+  ## plot the tree with different colors for different alleles
+  ##
+  p_snp <- ggtree(snp_groups, aes(color = group)) +
+    scale_color_brewer(palette = "Dark2") +
+    geom_tiplab(size = 1.5) +
+    theme_tree2() +
+    ggtitle("Colored by alleles")
+}
 
 ## associate haplotypes with populations
 ##
 pops <- character(0)
 for (i in 1:length(pop_config)) {
-  pops <- c(pops, rep(LETTERS[i], pop_config[i]))
+  pops <- c(pops, rep(paste0("Pop", LETTERS[i]), pop_config[i]))
 }
-## collect information for the tree
+if (0) {
+  ## collect information for the tree
+  ##
+  group_info <- split(tree$tip.label, pops)
+  pop_groups <- groupOTU(tree, group_info)
+  ## plot the tree with different colors for different populations
+  ##
+  p_pop <- ggtree(pop_groups, aes(color = group)) +
+    scale_color_brewer(palette = "Dark2") +
+    geom_tiplab(size = 1.5) +
+    theme_tree2() +
+    ggtitle("Colored by populations")
+}
+
+if (0) {
+  print(plot_grid(p_snp, p_pop))
+}
+
+## construct base tree
 ##
-group_info <- split(tree$tip.label, pops)
-snp_groups <- groupOTU(tree, group_info)
-## plot the tree with different colors for different populations
+p <- ggtree(tree) +
+  theme_tree2()
+## before adding allele and population information to it
 ##
-p <- ggtree(snp_groups, aes(color = group)) +
-  scale_color_brewer(palette = "Dark2") +
-  geom_tiplab(size = 1.5) +
-  ggtitle("Colored by populations")
+for_plot <- data.frame(taxa = tree$tip.label,
+                       Allele = haplos_char,
+                       Population = pops)
+p <- p %<+% for_plot +
+  geom_tippoint(aes(color = Allele, shape = Population)) +
+  theme(legend.position = "right")
 print(p)
